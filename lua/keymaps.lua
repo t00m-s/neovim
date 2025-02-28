a9711acbb67771496d71485aa2544da0c10d672d
Tommaso Soncin <soncintommaso@gmail.com>
2025-02-28 10:42:54 +0100

    feat: usign snacks for picking files

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -67,13 +67,19 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Select all
 vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select all' })
 
+-- Oil
+vim.keymap.set('n', '<leader>pv', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
 -- Split management
 vim.keymap.set('n', '<leader>sv', '<CMD>vsp<CR>', { noremap = true, silent = true, desc = '[S]plit [V]ertical' })
 vim.keymap.set('n', '<leader>sh', '<CMD>sp<CR>', { noremap = true, silent = true, desc = '[S]plit [H]orizontal' })
-vim.keymap.set('n', '<C-s>', '<CMD>w<CR>', { noremap = true, silent = true, desc = 'Save file.' })
+
 -- Move chunks of text
 vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set('v', '<A-h>', '<gv', { noremap = true, silent = true })
 vim.keymap.set('v', '<A-l>', '>gv', { noremap = true, silent = true })
+
+-- Unsetting default LSP keymaps
+vim.cmd [[nnoremap <nowait> gr gr]]
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,9 +2,6 @@ return {
   -- Main LSP Configuration
   'neovim/nvim-lspconfig',
   dependencies = {
-    { 'williamboman/mason.nvim', opts = {} },
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
     -- Useful status updates for LSP.
     { 'j-hui/fidget.nvim', opts = {} },
   },
@@ -119,25 +116,8 @@ return {
         },
       },
     }
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-    })
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
+    for server_name, server_config in pairs(servers) do
+      require('lspconfig')[server_name].setup(server_config)
+    end
   end,
 }
