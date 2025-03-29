e14fde868e5e371955760653f71692bf6e2d2b87
Tommaso Soncin <soncintommaso@gmail.com>
2025-03-29 12:09:55 +0100

    fix: double lsp start

--- lua/lsp.lua
+++
@@ -1,5 +0,0 @@
-vim.lsp.config('*', {
-  root_markers = { '.git' },
-  capabilities = require('blink.cmp').get_lsp_capabilities(),
-})
-vim.lsp.enable { 'luals', 'gopls', 'clangd', 'bashls', 'ruff', 'basedpyright' }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,25 +7,11 @@ return {
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
-        -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-        ---@param client vim.lsp.Client
-        ---@param method vim.lsp.protocol.Method
-        ---@param bufnr? integer some lsp support methods only in specific files
-        ---@return boolean
-        local function client_supports_method(client, method, bufnr)
-          if vim.fn.has 'nvim-0.11' == 1 then
-            return client:supports_method(method, bufnr)
-          else
-            return client.supports_method(method, { bufnr = bufnr })
-          end
-        end
-
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if
           client
-          and client_supports_method(
-            client,
+          and client:supports_method(
             vim.lsp.protocol.Methods.textDocument_documentHighlight,
             event.buf
           )
@@ -57,36 +43,6 @@ return {
         end
       end,
     })
-
-    -- Diagnostic Config
-    -- See :help vim.diagnostic.Opts
-    vim.diagnostic.config {
-      severity_sort = true,
-      float = { border = 'rounded', source = 'if_many' },
-      underline = { severity = vim.diagnostic.severity.ERROR },
-      signs = vim.g.have_nerd_font and {
-        text = {
-          [vim.diagnostic.severity.ERROR] = '󰅚 ',
-          [vim.diagnostic.severity.WARN] = '󰀪 ',
-          [vim.diagnostic.severity.INFO] = '󰋽 ',
-          [vim.diagnostic.severity.HINT] = '󰌶 ',
-        },
-      } or {},
-      virtual_text = {
-        source = 'if_many',
-        spacing = 2,
-        format = function(diagnostic)
-          local diagnostic_message = {
-            [vim.diagnostic.severity.ERROR] = diagnostic.message,
-            [vim.diagnostic.severity.WARN] = diagnostic.message,
-            [vim.diagnostic.severity.INFO] = diagnostic.message,
-            [vim.diagnostic.severity.HINT] = diagnostic.message,
-          }
-          return diagnostic_message[diagnostic.severity]
-        end,
-      },
-    }
-
     local servers = {
       lua_ls = {
         cmd = { 'lua-language-server' },
