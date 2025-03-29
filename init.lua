4487067a813c12edcefe7dfb917ee4e04125be1d
Tommaso Soncin <soncintommaso@gmail.com>
2025-03-29 12:07:35 +0100

    removed lsp dir

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 -- [[ Setting options ]]
 require 'options'
@@ -14,7 +13,4 @@ require 'lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
-
-require 'lsp'
--- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
