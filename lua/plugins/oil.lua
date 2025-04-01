2837c72e3640b81bee0b7665b0ffc764e3a5a23b
Tommaso Soncin <soncintommaso@gmail.com>
2025-04-01 21:37:13 +0200

    chore: removed double dependency

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,25 +1,30 @@
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  dependencies = { 'echasnovski/mini.icons' },
+  dependencies = { 'echasnovski/mini.nvim' },
   event = 'VimEnter',
-  config = function()
-    ---@type oil.SetupOpts
-    local opts = {
-      columns = {
-        'icon',
-        'permissions',
-        'size',
-      },
-      watch_for_changes = true,
-      use_default_keymaps = false,
-      keymaps = {
-        ['<CR>'] = 'actions.select',
-      },
-      view_options = { show_hidden = true },
-      default_file_explorer = true,
-    }
-    require('oil').setup(opts)
-    vim.keymap.set('n', '<leader>pv', '<CMD>Oil<CR>', { desc = '[P]roject [V]iew' })
-  end,
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    columns = {
+      'icon',
+      'permissions',
+      'size',
+    },
+    watch_for_changes = true,
+    use_default_keymaps = false,
+    keymaps = {
+      ['<CR>'] = 'actions.select',
+    },
+    view_options = { show_hidden = true },
+    default_file_explorer = true,
+  },
+  keys = {
+    {
+      '<leader>pv',
+      function()
+        require('oil').open()
+      end,
+      desc = '[P]roject [V]iew',
+    },
+  },
 }
