b0f39c4366bd7d628f5cb01c095c430867eca238
Tommaso Soncin <soncintommaso@gmail.com>
2025-04-08 17:48:45 +0200

    adding options to fzf-lua

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -2,7 +2,11 @@ return {
   'ibhagwan/fzf-lua',
   dependencies = { 'echasnovski/mini.icons' },
   event = { 'VeryLazy' },
-  opts = {},
+  opts = {
+    oldfiles = { include_current_session = true },
+    previewers = { builtin = { syntax_limit_b = 1024 * 100 } },
+    grep = { rg_glob = true, glob_flag = '--iglob' },
+  },
   keys = {
     {
       '<leader>sf',
