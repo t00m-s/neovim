6647fab712f844e01d147f5ed328346e8b217c5b
Tommaso Soncin <soncintommaso@gmail.com>
2025-04-14 14:31:04 +0200

    using notifications instead of fidget

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -28,3 +28,33 @@ vim.api.nvim_create_autocmd('InsertLeave', {
     }
   end,
 })
+
+-- Create a minimal LSP status notification system
+local lsp_notify = vim.api.nvim_create_augroup('LspNotify', { clear = true })
+
+-- Track active clients
+local active_clients = {}
+
+-- When an LSP attaches
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = lsp_notify,
+  callback = function(args)
+    local client_id = args.data.client_id
+    local client = vim.lsp.get_client_by_id(client_id)
+    if client then
+      active_clients[client_id] = client.name
+      vim.notify('LSP started: ' .. client.name, vim.log.levels.INFO)
+    end
+  end,
+})
+
+-- When an LSP detaches
+vim.api.nvim_create_autocmd('LspDetach', {
+  group = lsp_notify,
+  callback = function(args)
+    local client_id = args.data.client_id
+    local client_name = active_clients[client_id] or 'Unknown'
+    active_clients[client_id] = nil
+    vim.notify('LSP stopped: ' .. client_name, vim.log.levels.INFO)
+  end,
+})
