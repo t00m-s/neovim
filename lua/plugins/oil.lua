e619d82974b2e78f5cdfbb0344e1a3c44cf88607
Tommaso Soncin <soncintommaso@gmail.com>
2025-07-02 14:59:35 +0200

    feat: visual improvements of oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,28 +1,46 @@
 return {
-  'stevearc/oil.nvim',
-  dependencies = { 'echasnovski/mini.nvim' },
-  event = 'VimEnter',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
-    columns = {
-      'icon',
-      'permissions',
-      'size',
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'echasnovski/mini.nvim' },
+    event = 'VimEnter',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      columns = {
+        'icon',
+        'permissions',
+        'size',
+      },
+      watch_for_changes = true,
+      use_default_keymaps = false,
+      keymaps = {
+        ['<CR>'] = 'actions.select',
+      },
+      view_options = { show_hidden = true },
+      default_file_explorer = true,
+      win_options = {
+        signcolumn = 'yes:2',
+      },
     },
-    watch_for_changes = true,
-    use_default_keymaps = false,
-    keymaps = {
-      ['<CR>'] = 'actions.select',
+    keys = {
+      {
+        '<leader>pv',
+        function() require('oil').toggle_float() end,
+        desc = '[P]roject [V]iew',
+      },
     },
-    view_options = { show_hidden = true },
-    default_file_explorer = true,
   },
-  keys = {
-    {
-      '<leader>pv',
-      function() require('oil').toggle_float() end,
-      desc = '[P]roject [V]iew',
+  {
+    'refractalize/oil-git-status.nvim',
+
+    dependencies = {
+      'stevearc/oil.nvim',
     },
+    opts = {},
+  },
+  {
+    'JezerM/oil-lsp-diagnostics.nvim',
+    dependencies = { 'stevearc/oil.nvim' },
+    opts = {},
   },
 }
