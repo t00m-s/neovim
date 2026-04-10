9f3d0251f3ec30a9d66f94dd74bafb984cf0eec7
Tommaso Soncin <tommaso.soncin@protonmail.com>
2026-04-10 11:39:06 +0200

    chore: readded vim-suda

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -6,6 +6,7 @@ vim.pack.add {
   { src = 'https://github.com/stevearc/conform.nvim' },
   { src = 'https://github.com/neovim/nvim-lspconfig' }, -- Optional, but saves work
   { src = 'https://github.com/folke/flash.nvim' },
+  { src = 'https://github.com/lambdalisue/vim-suda' },
 }
 require('mason').setup {}
 require('trouble').setup {
